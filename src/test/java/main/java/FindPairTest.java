97a244dc53b82836ad1bcc9bd3e16c11891b319a
Anton Asela <to.anton.asela@gmail.com>
2022-08-28 11:07:44 +0800

    update unit test

--- src/test/java/main/java/FindPairTest.java
+++ src/test/java/main/java/FindPairTest.java
@@ -3,9 +3,7 @@ package main.java;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 import java.util.Set;
 import java.util.random.RandomGenerator;
 import java.util.stream.Collectors;
@@ -21,70 +19,68 @@ class FindPairTest {
         pair = new FindPair();
     }
 
-    private void testFindPair_noSolution(int[] values, int limit) {
-        Set ret = pair.findPair(values, limit);
+    private void testFindPair_noSolution(int target, int... values) {
+        Set ret = pair.findPair(values, target);
         assertNotNull(ret);
         assertInstanceOf(Set.class,ret);
         assertTrue(ret.isEmpty());
     }
 
-    private void testFindPair(int[] input, int limit) {
-        Set<Integer> pair = this.pair.findPair(input, limit);
+    private void testFindPair(int target, int... input) {
+        Set<Integer> pair = this.pair.findPair(input, target);
         assertNotNull(pair);
         assertEquals(2, pair.size());
-        assertEquals(limit,pair.stream().mapToInt(Integer::intValue).sum());
+        assertEquals(target,pair.stream().mapToInt(Integer::intValue).sum());
         assertTrue(Arrays.stream(input).boxed().collect(Collectors.toSet()).containsAll(pair));
     }
 
     @Test
     void shouldReturnEmpty_whenInputIsNull() {
-        testFindPair_noSolution(null, 0);
+        testFindPair_noSolution(0, null);
     }
 
     @Test
     void shouldReturnEmpty_whenNoMatchingPairs() {
-        testFindPair_noSolution(new int[]{1, 2, 5, 4}, 200);
+        testFindPair_noSolution(200, 1, 2, 5, 4);
     }
 
     @Test
     void shouldReturnMatchingPair_whenArraySize_Two() {
-        testFindPair(new int[]{1, 2}, 3);
+        testFindPair(3, 1, 2);
     }
 
     @Test
     void shouldReturnMatchingPair_whenArraySize_Three() {
-        testFindPair(new int[]{1, 2, 5}, 6);
+        testFindPair(6, 1, 2, 5);
     }
 
     @Test
     void shouldReturnMatchingPair_whenArraySize_Four() {
-        testFindPair(new int[]{0, 2, 5, 4}, 6);
+        testFindPair(6, 0, 2, 5, 4);
     }
 
     @Test
     void shouldReturnOnlyOneMatchingPair_whenTwoMatchingPairsExits() {
-        testFindPair(new int[]{1, 2, 5, 4}, 6);
+        testFindPair(6, 1, 2, 5, 4);
     }
 
     @Test
     void challenge_test1() {
-        testFindPair(new int[]{8, 7, 5, 2, 3, 1}, 10);
+        testFindPair(10, 8, 7, 5, 2, 3, 1);
     }
 
     @Test
     void challenge_test2() {
-        testFindPair_noSolution(new int[]{5, 2, 6, 8, 1, 9}, 12);
+        testFindPair_noSolution(12, 5, 2, 6, 8, 1, 9);
     }
 
     @Test
-    void performace() {
-
-
+    void check_performance() {
         int[] input = RandomGenerator.getDefault().ints(1, 20000)
                 .limit(1000000).toArray();
         input[input.length - 1] = 100_000_000;
         input[input.length - 2] = 100_000_001;
-        testFindPair(input, 200_000_001);
+        testFindPair(200_000_001, input);
     }
 
 }
